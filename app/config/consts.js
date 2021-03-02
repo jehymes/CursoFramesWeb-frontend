14865d326e5aaea85e433b7519f6f9710d8d550a
Jehymeson Gil <jehymes.gil@live.com>
2021-03-02 14:29:41 -0300

    task 0009

--- app/config/consts.js
+++ app/config/consts.js
@@ -4,8 +4,8 @@ angular.module('primeiraApp').constant('consts', {
   owner: 'JG Desenvolvimentos',
   year: '2021',
   site: 'https://jgdesenvolvimentos.com.br',
-  apiUrl: 'https://admin-money-backend.herokuapp.com/api',
-  oapiUrl: 'https://admin-money-backend.herokuapp.com/oapi',
+  apiUrl: /*'https://admin-money-backend.herokuapp.com/api'*/ 'http://localhost:3003/api',
+  oapiUrl: /*'https://admin-money-backend.herokuapp.com/oapi'*/ 'http://localhost:3003/oapi',
   userKey: '_primeira_app_user'
 }).run(['$rootScope', 'consts', function($rootScope, consts) {
   $rootScope.consts = consts
